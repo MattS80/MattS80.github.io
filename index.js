a8921e6fc26a01c39c14cd086ac02af57db283b2
MattS80 <Matthias-Schrimpf@gmx.de>
2024-05-31 14:04:34 +0200

    add utility for query Selector

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 var activePage = "home";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
 function show(id) {
-  document.getElementById(id).style.display = "block";
+  //document.getElementById(id).style.display = "block";
+  $("#" + id).style.display = "block";
 }
 
 function hide(id) {
-  document.getElementById(id).style.display = "none";
+  //document.getElementById(id).style.display = "none";
+  $(`#${id}`).style.display = "none";
+  //Alternative Schreibweise
+}
+
+function SetActiveFlagOfactivePage(add) {
+  if (add) {
+    $(`#top-menu-bar a[data-page="${activePage}"]`).classList.add("active");
+  } else {
+    $(`#top-menu-bar a[data-page="${activePage}"]`).classList.remove("active");
+  }
 }
 
 function showPage(id) {
   hide(activePage);
-  document
-    .querySelector(`#top-menu-bar a[data-page="${activePage}"]`)
-    .classList.remove("active");
+  SetActiveFlagOfactivePage(false);
   show(id);
   activePage = id;
-  document
-    .querySelector(`#top-menu-bar a[data-page="${activePage}"]`)
-    .classList.add("active");
+  SetActiveFlagOfactivePage(true);
 }
 
 showPage(activePage);
 
-document.querySelector("#top-menu-bar").addEventListener("click", function (e) {
+$("#top-menu-bar").addEventListener("click", function (e) {
   var id = e.target.dataset.page;
   //console.info("Click on menu-bar", e.target.getAttribute("data-page"));
   //show(e.target.getAttribute("data-page"));
